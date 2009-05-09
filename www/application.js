237246b4716c0f9851527512872c38c525b54b2f
Rob Ellis <kazoomer@gmail.com>
2009-05-08 22:36:39 -0700

    Big Update

--- www/application.js
+++ www/application.js
@@ -2,9 +2,21 @@ x$(window).on('load',function(){
 	
 	var cb = function(d) {		
 		var data = eval('(' + this.responseText + ')');	
-		console.log(data)	;
 
+		// Speakers
+		for (var i = 0; i < data.sessions.length; i++) {
+			x$('#speakers ul').top("<li><h1>" + data.sessions[i].speaker.name + "</h1><p>"+ data.sessions[i].speaker.bio +"</p></li>");
+		}
+		
+		// Schedule
+		for (var i = 0; i < data.sessions.length; i++) {
+			x$('#schedule ul').top("<li><h1>" + data.sessions[i].speaker.name + "</h1><h3>"+ data.sessions[i].title +"</h3><p>"+data.sessions[i].description +"</p></li>");
+		}
+		
 	}
+
+
+	
 
 	x$('#index').xhr('data.json', {callback : cb } );
 });
